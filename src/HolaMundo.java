6da3f4d469113daf69fd060a83ca147904000fd6
Miguel18201 <146874956+Miguel18201@users.noreply.github.com>
2023-10-03 12:40:54 -0600

    Creación de practica 1

---
+++ src/HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * Preyecto con Maven
+ * Autor: Sanchez Ramirez Miguel Angel
+ */
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+    
+}
---
+++ src/main/java/package1/HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package package1;
+
+/**
+ *
+ * Sanchez Ramirez Miguel Angel
+ */
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo ");
+    }
+}
